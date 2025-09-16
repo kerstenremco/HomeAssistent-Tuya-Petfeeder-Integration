6855001df68d943c13c3fe8c1f3ae1e7aad0ab60
Remco Kersten <kersten.remco@outlook.com>
2025-09-16 14:34:15 +0200

    Added convert plan

--- custom_components/petfeeder/feeder.py
+++ custom_components/petfeeder/feeder.py
@@ -1,6 +1,6 @@
 import tinytuya
 import asyncio
-import json
+import base64
 
 
 class Feeder:
@@ -31,3 +31,36 @@ class Feeder:
         loop = asyncio.get_running_loop()
         status = await loop.run_in_executor(None, self.device.status)
         return 'dps' in status
+
+    def _decode_plan(self):
+        # TODO: implement fetching, https://community.home-assistant.io/t/pet-feeder-entities/454077/60
+        raw = base64.b64decode()
+
+        # Feeding plan = 6 groups × 5 bytes = 30 bytes
+        plans = []
+        for i in range(0, len(raw), 5):
+            group = raw[i:i+5]
+            if len(group) < 5:
+                continue
+
+            day, hour, minute, servings, switch = group
+
+            # Skip empty (zero-filled) groups
+            if all(v == 0 for v in group):
+                continue
+
+            days_bin = f"{day:07b}"
+            days = []
+            week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+            for j, w in enumerate(week):
+                if days_bin[j] == "1":
+                    days.append(w)
+
+            plans.append({
+                "days": days,
+                "time": f"{hour:02d}:{minute:02d}",
+                "servings": servings,
+                "enabled": (switch == 1)
+            })
+
+        return plans
